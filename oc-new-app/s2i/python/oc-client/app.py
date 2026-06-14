46a07f9c53401b07c4b6482e63987f943853afb6
ninadaa <33172436+ninadaa@users.noreply.github.com>
2026-06-14 12:01:55 +0200

    Update app.py

--- oc-new-app/s2i/python/oc-client/app.py
+++ oc-new-app/s2i/python/oc-client/app.py
@@ -1,22 +1,15 @@
-#!/usr/bin/python
-import time
-try:
-    import openshift_client as oc
-    print("openshift_client installed")
-except ImportError:
-    print("openshift_client not installed")
-    time.sleep(1200)
+#!/usr/bin/env python
 
-print('OpenShift client version: {}'.format(oc.get_client_version()))
-print('OpenShift server version: {}'.format(oc.get_server_version()))
+from __future__ import absolute_import
+from __future__ import print_function
 
-current_project = oc.get_project_name()
-print(f"Aktuelles Projekt: {current_project}")
+import openshift_client as oc
+from openshift_client import OpenShiftPythonException
 
-# Set a project context for all inner `oc` invocations and limit execution to 10 minutes
-with oc.project(current_project), oc.timeout(10 * 60):
-
-    while True:
-        # Print the list of qualified pod names (e.g. ['pod/xyz', 'pod/abc', ...]  in the current project
-        print('Found the following pods in {}: {}'.format(oc.get_project_name(), oc.selector('pods').qnames()))
-        time.sleep(10)
+if __name__ == '__main__':
+    with oc.tracking() as tracker:
+        try:
+            print('Current project: {}'.format(oc.get_project_name()))
+            print('Current user: {}'.format(oc.whoami()))
+        except OpenShiftPythonException as e:
+            print('Error acquiring details about project/user: {}'.format(e))
