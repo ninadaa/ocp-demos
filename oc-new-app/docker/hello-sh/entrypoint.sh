2340fb1ca64b3b8cbc799cb1f74e48b42d56d9c4
ninadaa <33172436+ninadaa@users.noreply.github.com>
2026-06-19 12:30:00 +0200

    Update entrypoint.sh

--- oc-new-app/docker/hello-sh/entrypoint.sh
+++ oc-new-app/docker/hello-sh/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -Eeuo pipefail 
 
+echo "Wer bin ich?"
 id
+echo "Wo stehe ich?"
 pwd
+echo "Meine Umgebung?"
+echo "env:"
+env | sort
+
+echo "Starte Endlosschleife..."
 while true; do echo "Hello from appuser in container"; sleep 60; done
