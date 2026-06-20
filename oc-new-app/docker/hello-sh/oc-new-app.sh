817cc430837d9fccab9a5924087e34f398cc04fb
ninadaa <33172436+ninadaa@users.noreply.github.com>
2026-06-20 13:31:45 +0200

    Delete oc-new-app.sh

--- oc-new-app/docker/hello-sh/oc-new-app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail 
-
-echo "Running Docker Build in $(oc project -q)"
-oc new-app https://github.com/ninadaa/ocp-demos.git --context-dir=oc-new-app/docker/hello-sh --name=hello-sh --strategy=docker
